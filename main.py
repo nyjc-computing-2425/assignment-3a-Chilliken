7c7245d3d67f36b2731a417317403850c051de5b
Chilliken <74310036+Chilliken@users.noreply.github.com>
2024-03-20 01:57:01 +0000

    done

--- main.py
+++ main.py
@@ -4,29 +4,46 @@ from re import T
 nric = input('Enter an NRIC number: ')
 validity = "invalid"
 # Type your code below
+prefix = nric[0]
+numbers = nric[1:8]
+suffix = nric[-1]
+first_letter = "STFG"
 digit_weight = "2765432"
 st_digit = "JZIHGFEDCBA"
 fg_digit = "XWUTRQPNMLK"
-validity = "invalid"
-sum = 0
-for i in nric:
-    if i.isnumeric() :
-        i = int(i)
-        if i > 0 and i <8 :
-            sum +=i*int(digit_weight[i-1])
-checkdigit = sum%11
-if nric[0] == "S" or nric[0] == "T":
-    checking = nric[8]
-    if checking == st_digit[checkdigit]:
-        validity = "valid"
-if nric[0] == "F" or nric[0] == "G":
-    checking = nric[8]
-    if checking == fg_digit[checkdigit]:
-        validity = "valid"
-if nric[0] != "F" and nric[0] != "G" and nric[0] != "S" and nric[0] != "T":
-    validity = "invalid"
-if len(nric) != 9:
-    validity = "invalid"
-
+invalidity = "NRIC is invalid."
+validity = "NRIC is valid."
 print(f"The input NRIC is {nric}.")
-print(f"NRIC is {validity}.")
+if prefix not in first_letter:
+    print(invalidity)
+elif len(nric) != 9:
+    print(invalidity)
+elif numbers.isnumeric() is False:
+    print(invalidity)
+elif numbers.isnumeric():
+    sum = (int(numbers[0]) * 2
+          + int(numbers[1]) * 7
+          + int(numbers[2]) * 6
+          + int(numbers[3]) * 5
+          + int(numbers[4]) * 4
+          + int(numbers[5]) * 3
+          + int(numbers[6]) * 2)
+    
+    if prefix in "TG":
+        sum +=4
+    
+    remainder = sum % 11
+    
+    checkdigit = ''
+    if prefix in "ST":
+        checkdigit = st_digit[remainder]
+    elif prefix in "FG":
+        checkdigit = fg_digit[remainder]
+    
+    if suffix == checkdigit:
+        print(validity)
+    else:
+        print(invalidity)
+
+    
+
